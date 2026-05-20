a974f6a672353a575e4fd7ed1cc8da892a67b3ee
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-20 21:51:40 +0530

    Add logical_operator.java to demonstrate logical operations in Java

---
+++ Day 02/Basic java/logical_operator.java
@@ -0,0 +1,12 @@
+public class logical_operator {
+    public static void main(String[] args) {
+        // AND operator
+        System.out.println((4<6)&&(3<9));
+
+        // OR operator 
+        System.out.println((5>9)||(4<8));
+
+        // NOT operator
+        System.out.println(!(4<6));
+    }
+}
