d91e7729eb369eb23df35fb89ebf5d1c1c53ac35
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-20 21:45:55 +0530

    Add Data_type.java to demonstrate basic data types in Java

---
+++ Day 01/Basic Java/Data_type.java
@@ -0,0 +1,15 @@
+public class Data_type {
+    public static void main(String[] args) {
+        // int 
+        int a=10;  // here int is data type of variable a
+        System.out.println("integer:"+a);
+
+        // string
+        String place="boisar"; //here String is a datatype of place variable 
+        System.out.println("String:"+place);
+
+        // double
+        double b=4.5;  // double datatype is used for deecimal variables
+        System.out.println("double:"+b);
+    }
+}
