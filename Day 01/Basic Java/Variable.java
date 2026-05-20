d284060c4513b6d5df6dd9d8438eafc995b01bf2
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-20 21:48:53 +0530

    Add Variable.java to demonstrate variable initialization in Java

---
+++ Day 01/Basic Java/Variable.java
@@ -0,0 +1,8 @@
+class Variable{
+    // main function
+    public static void main (String[] args){
+        //initialize the variable a
+        int a=10;
+        System.out.println("the value of a is:"+a);
+    }
+}
