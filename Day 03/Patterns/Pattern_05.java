05ec8a1390830216b4d5383f0eec390980e93e0d
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-22 00:09:34 +0530

    Add Pattern_05.java to implement a program that prints a decreasing number pattern of asterisks

---
+++ Day 03/Patterns/Pattern_05.java
@@ -0,0 +1,17 @@
+// *****
+// ****
+// ***
+// **
+// *
+public class Pattern_05 {
+    public static void main(String[] args) {
+        // outer loop
+        for(int i=1;i<=5;i++){
+            // inner loop
+            for(int j=5;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
