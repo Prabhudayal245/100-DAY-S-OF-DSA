0fab80aece1a1fdedf2cf1a0e0294759aa309a6a
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-21 23:55:31 +0530

    Add do_while_loop.java to demonstrate the use of do-while loops in Java

---
+++ Day 02/Basic java/do_while_loop.java
@@ -0,0 +1,11 @@
+public class do_while_loop {
+
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i+")Hello world");
+            i++;
+        }
+        while(i<11);
+    }
+}
