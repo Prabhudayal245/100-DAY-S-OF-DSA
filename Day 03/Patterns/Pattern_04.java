ebd42b84b7e1a931656dbfdc12d738b0f3fd62c5
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-22 00:09:21 +0530

    Add Pattern_04.java to implement a program that prints a number pattern

---
+++ Day 03/Patterns/Pattern_04.java
@@ -0,0 +1,17 @@
+// 1
+// 22
+// 333
+// 4444
+// 55555
+public class Pattern_04 {
+    public static void main(String[] args) {
+        // outer loop
+        for(int i=1;i<=5;i++){
+            // inner loop
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
