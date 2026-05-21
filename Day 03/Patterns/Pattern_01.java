bbd0da79785a3fa238910641f92ac61a953571a0
Prabhudayal <prabhudayaly005@gmail.com>
2026-05-21 23:56:08 +0530

    Add Pattern_01.java to implement a program that prints a square pattern of asterisks

---
+++ Day 03/Patterns/Pattern_01.java
@@ -0,0 +1,16 @@
+// ****
+// ****
+// ****
+// ****
+public class Pattern_01 {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){ // outer loop
+
+            for(int j=0;j<4;j++){  // inner loop
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }
+    }
+}
